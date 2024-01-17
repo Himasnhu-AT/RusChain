b2ad9b93bdd524f27e469c51bb9b36a08b2ffd3e
Himanshu-AT <aristech.official@gmail.com>
2024-01-17 13:37:22 +0530

    demo demo dev-docs

---
+++ dev-docs/.vitepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: "RusChain",
+    description: "Experimental 2-layered blockchain",
+    themeConfig: {
+        nav: [
+            { text: "Home", link: "/" },
+        ],
+        sidebar: [
+            {
+                text: "Introduction",
+                items: [
+                    {
+                        text: "get started",
+                        link: "/"
+                    },
+                ]
+            }
+        ]
+    }
+};
